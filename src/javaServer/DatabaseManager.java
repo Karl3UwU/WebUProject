e300f9d551f938208822a037cb42f15db46133ac
Alex Tudor <alextudor6717@gmail.com>
2025-05-23 14:48:28 +0300

    Added more DatabaseManager methods

--- src/javaServer/DatabaseManager.java
+++ src/javaServer/DatabaseManager.java
@@ -257,7 +257,83 @@ public class DatabaseManager {
         }
         return books;
     }
+    // adds a new book
+    public boolean addBook(String title, String publisher, int yearPublished, int categoryId, int authorId, String edition) {
+        String query = "INSERT INTO books (title, publisher, year_published, category_id, author_id, edition) VALUES (?, ?, ?, ?, ?, ?)";
+        
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, title);
+            preparedStatement.setString(2, publisher);
+            preparedStatement.setInt(3, yearPublished);
+            preparedStatement.setInt(4, categoryId);
+            preparedStatement.setInt(5, authorId);
+            preparedStatement.setString(6, edition);
+            
+            int rowsInserted = preparedStatement.executeUpdate();
+            return rowsInserted > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
+    // --------------------------- ReadingList methods --------------------------- //
+    // returns a list of books read by an user id
+    public List<Map<String, Object>> getBooksReadByUser(int userId) {
+        List<Map<String, Object>> books = new ArrayList<>();
+        String query = "SELECT book_id, user_id FROM readinglist WHERE user_id = ?";
+        
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, userId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            
+            while (resultSet.next()) {
+                Map<String, Object> book = new HashMap<>();
+                book.put("book_id", resultSet.getInt("book_id"));
+                book.put("user_id", resultSet.getInt("user_id"));
+                books.add(book);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return books;
+    }
+    // adds a book to the reading list
+    public boolean addBookToReadingList(int userId, int bookId, String status) {
+        String query = "INSERT INTO reading_ist (user_id, book_id, status) VALUES (?, ?, ?)";
+        
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, userId);
+            preparedStatement.setInt(2, bookId);
+            preparedStatement.setString(3, status);
+
+            int rowsInserted = preparedStatement.executeUpdate();
+            return rowsInserted > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    // removes a book from the reading list
+    public boolean removeBookFromReadingList(int userId, int bookId) {
+        String query = "DELETE FROM readinglist WHERE user_id = ? AND book_id = ?";
+        
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, userId);
+            preparedStatement.setInt(2, bookId);
+            
+            int rowsDeleted = preparedStatement.executeUpdate();
+            return rowsDeleted > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
     
     // --------------------------- Author methods --------------------------- //
     // returns a list of all authors
@@ -272,10 +348,8 @@ public class DatabaseManager {
             while (resultSet.next()) {
                 Map<String, Object> author = new HashMap<>();
                 author.put("author_id", resultSet.getInt("author_id"));
-                author.put("first_name", resultSet.getString("first_name"));
-                author.put("last_name", resultSet.getString("last_name"));
-                author.put("biography", resultSet.getString("biography"));
-                author.put("birth_date", resultSet.getDate("birth_date"));
+                author.put("name", resultSet.getString("name"));
+                author.put("bio", resultSet.getString("bio"));
                 authors.add(author);
             }
         } catch (SQLException e) {
@@ -295,23 +369,36 @@ public class DatabaseManager {
             
             if (resultSet.next()) {
                 author.put("author_id", resultSet.getInt("author_id"));
-                author.put("first_name", resultSet.getString("first_name"));
-                author.put("last_name", resultSet.getString("last_name"));
-                author.put("biography", resultSet.getString("biography"));
-                author.put("birth_date", resultSet.getDate("birth_date"));
+                author.put("name", resultSet.getString("name"));
+                author.put("bio", resultSet.getString("bio"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return author;
     }
-    
+    // adds an autor
+    public boolean addAuthor(String name, String bio) {
+        String query = "INSERT INTO authors (name, bio) VALUES (?, ?)";
+        
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, bio);
+            
+            int rowsInserted = preparedStatement.executeUpdate();
+            return rowsInserted > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
     // --------------------------- Category methods --------------------------- //
     // returns a list of all categories
     public List<Map<String, Object>> getAllCategories() {
         List<Map<String, Object>> categories = new ArrayList<>();
-        String query = "SELECT * FROM book_categories";
+        String query = "SELECT * FROM categories";
         
         try (Connection connection = getConnection();
              Statement statement = connection.createStatement();
@@ -321,7 +408,6 @@ public class DatabaseManager {
                 Map<String, Object> category = new HashMap<>();
                 category.put("category_id", resultSet.getInt("category_id"));
                 category.put("category_name", resultSet.getString("category_name"));
-                category.put("description", resultSet.getString("description"));
                 categories.add(category);
             }
         } catch (SQLException e) {
@@ -332,7 +418,7 @@ public class DatabaseManager {
     // returns a category by id
     public Map<String, Object> getCategoryById(int categoryId) {
         Map<String, Object> category = new HashMap<>();
-        String query = "SELECT * FROM book_categories WHERE category_id = ?";
+        String query = "SELECT * FROM categories WHERE category_id = ?";
         
         try (Connection connection = getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
@@ -342,7 +428,6 @@ public class DatabaseManager {
             if (resultSet.next()) {
                 category.put("category_id", resultSet.getInt("category_id"));
                 category.put("category_name", resultSet.getString("category_name"));
-                category.put("description", resultSet.getString("description"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
