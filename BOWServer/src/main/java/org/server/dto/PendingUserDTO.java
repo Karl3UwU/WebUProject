23d2eb1b6f0ef98e007752e1381a175c08522a6b
Karl3UwU <coteleacarlo@gmail.com>
2025-06-20 01:53:13 +0300

    working partial email confirmations

---
+++ BOWServer/src/main/java/org/server/dto/PendingUserDTO.java
@@ -0,0 +1,67 @@
+package org.server.dto;
+
+public class PendingUserDTO {
+    long expiresAt;
+    String verificationCode;
+    String username;
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+
+    public long getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(long expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    public String getVerificationCode() {
+        return verificationCode;
+    }
+
+    public void setVerificationCode(String verificationCode) {
+        this.verificationCode = verificationCode;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
